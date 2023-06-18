ebed8d70d12feaabce71ac8fee9f086b3227c4ea
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-06-18 11:03:01 -0400

    import sea valves from excel

--- app/Controllers/Clients.php
+++ app/Controllers/Clients.php
@@ -1874,6 +1874,288 @@ class Clients extends Security_Controller {
         return $this->template->view('clients/sea_valves/modal_form', $view_data);
     }
 
+    function import_sea_valves_modal_form() {
+        $this->access_only_allowed_members();
+
+        $client_id = $this->request->getPost('client_id') ? $this->request->getPost('client_id') : $view_data['model_info']->client_id;
+        $this->can_access_own_client($client_id);
+
+        $view_data['client_id'] = $client_id;
+
+        return $this->template->view('clients/sea_valves/import_modal_form', $view_data);
+    }
+
+    function save_sea_valve_from_excel_file() {
+        $client_id = $this->request->getPost('client_id');
+        $this->can_access_own_client($client_id);
+
+        if (!$this->validate_import_sea_valves_file_data(true)) {
+            echo json_encode(array('success' => false, 'message' => app_lang('error_occurred')));
+        }
+
+        $file_name = $this->request->getPost('file_name');
+        require_once(APPPATH . "ThirdParty/PHPOffice-PhpSpreadsheet/vendor/autoload.php");
+
+        $temp_file_path = get_setting("temp_file_path");
+        $excel_file = \PhpOffice\PhpSpreadsheet\IOFactory::load($temp_file_path . $file_name);
+        $excel_file = $excel_file->getActiveSheet()->toArray();
+
+        $allowed_headers = $this->_get_allowed_sea_valve_headers();
+
+        foreach ($excel_file as $key => $value) { //rows
+            if ($key === 0) { //first line is headers, continue for the next loop
+                continue;
+            }
+
+            $sea_valve_data = $this->_prepare_sea_valve_data($value, $allowed_headers);
+            //couldn't prepare valid data
+            if (!($sea_valve_data && count($sea_valve_data))) {
+                continue;
+            }
+            $sea_valve_data["client_id"] = $client_id;
+
+            //save client data
+            $save_id = $this->Sea_valves_model->ci_save($sea_valve_data);
+            if (!$save_id) {
+                continue;
+            }
+        }
+
+        delete_file_from_directory($temp_file_path . $file_name); //delete temp file
+
+        echo json_encode(array('success' => true, 'message' => app_lang("record_saved")));
+    }
+
+    function download_sample_sea_valves_excel_file() {
+        $this->access_only_allowed_members();
+        return $this->download_app_files(get_setting("system_file_path"), serialize(array(array("file_name" => "import-sea-valves-sample.xlsx"))));
+    }
+
+    function validate_import_sea_valves_file() {
+        $this->access_only_allowed_members();
+
+        $file_name = $this->request->getPost("file_name");
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+        if (!is_valid_file_to_upload($file_name)) {
+            echo json_encode(array("success" => false, 'message' => app_lang('invalid_file_type')));
+            exit();
+        }
+
+        if ($file_ext == "xlsx") {
+            echo json_encode(array("success" => true));
+        } else {
+            echo json_encode(array("success" => false, 'message' => app_lang('please_upload_a_excel_file') . " (.xlsx)"));
+        }
+    }
+
+    private function _get_allowed_sea_valve_headers() {
+        return array(
+            "name",
+            "description",
+            "norm",
+            "diameter_nominal",
+            "pressure_rating",
+            "length",
+            "height",
+            "diameter"
+        );
+    }
+
+    private function _store_sea_valve_headers_position($headers_row = array()) {
+        $allowed_headers = $this->_get_allowed_sea_valve_headers();
+
+        //check if all headers are correct and on the right position
+        $final_headers = array();
+        foreach ($headers_row as $key => $header) {
+            if (!$header) {
+                continue;
+            }
+
+            $key_value = str_replace(' ', '_', strtolower(trim($header, " ")));
+            $header_on_this_position = get_array_value($allowed_headers, $key);
+            $header_array = array("key_value" => $header_on_this_position, "value" => $header);
+
+            if ($header_on_this_position == $key_value) {
+                //allowed headers
+                //the required headers should be on the correct positions
+                //the rest headers will be treated as custom fields
+                //pushed header at last of this loop
+            } else { //invalid header, flag as red
+                $header_array["has_error"] = true;
+            }
+
+            if ($key_value) {
+                array_push($final_headers, $header_array);
+            }
+        }
+
+        return $final_headers;
+    }
+
+    private function _sea_valve_row_data_validation_and_get_error_message($key, $data) {
+        $allowed_headers = $this->_get_allowed_headers();
+        $header_value = get_array_value($allowed_headers, $key);
+
+        //there has no date field on default import fields
+        //check on custom fields
+        if (((count($allowed_headers) - 1) < $key) && $data) {
+            if ($header_value == "description") {
+
+            } else {
+                if (empty($data)) {
+                    $error_message = sprintf(app_lang("import_data_empty_message"), app_lang($header_value));
+                    return $error_message;
+                } else {
+                    if ($header_value == "name") {
+                        if (strlen($data) > 30) {
+                            $error_message = sprintf(app_lang("import_data_max_length_error_message"), app_lang($header_value), "30");
+                        }
+                    } else {
+                        if (strlen($data) > 20) {
+                            $error_message = sprintf(app_lang("import_data_max_length_error_message"), app_lang($header_value), "20");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function _prepare_sea_valve_data($data_row, $allowed_headers) {
+        //prepare sea valve data
+        $sea_valve_data = array();
+
+        foreach ($data_row as $row_data_key => $row_data_value) { //row values
+            if (!$row_data_value) {
+                continue;
+            }
+
+            $header_key_value = get_array_value($allowed_headers, $row_data_key);
+            $sea_valve_data[$header_key_value] = $row_data_value;
+        }
+
+        return $sea_valve_data;
+    }
+
+    function validate_import_sea_valves_file_data($check_on_submit = false) {
+        $this->access_only_allowed_members();
+
+        $table_data = "";
+        $error_message = "";
+        $headers = array();
+        $got_error_header = false; //we've to check the valid headers first, and a single header at a time
+        $got_error_table_data = false;
+
+        $file_name = $this->request->getPost("file_name");
+
+        require_once(APPPATH . "ThirdParty/PHPOffice-PhpSpreadsheet/vendor/autoload.php");
+
+        $temp_file_path = get_setting("temp_file_path");
+        $excel_file = \PhpOffice\PhpSpreadsheet\IOFactory::load($temp_file_path . $file_name);
+        $excel_file = $excel_file->getActiveSheet()->toArray();
+
+        $table_data .= '<table class="table table-responsive table-bordered table-hover" style="width: 100%; color: #444;">';
+
+        $table_data_header_array = array();
+        $table_data_body_array = array();
+
+        foreach ($excel_file as $row_key => $value) {
+            if ($row_key == 0) { //validate headers
+                $headers = $this->_store_sea_valve_headers_position($value);
+
+                foreach ($headers as $row_data) {
+                    $has_error_class = false;
+                    if (get_array_value($row_data, "has_error") && !$got_error_header) {
+                        $has_error_class = true;
+                        $got_error_header = true;
+
+                        $error_message = sprintf(app_lang("import_sea_valve_error_header"), app_lang(get_array_value($row_data, "key_value")));
+                    }
+
+                    array_push($table_data_header_array, array("has_error_class" => $has_error_class, "value" => get_array_value($row_data, "value")));
+                }
+            } else { //validate data
+                if (!array_filter($value)) {
+                    continue;
+                }
+
+                $error_message_on_this_row = "<ol class='pl15'>";
+
+                foreach ($value as $key => $row_data) {
+                    $has_error_class = false;
+
+                    if (!$got_error_header) {
+                        $row_data_validation = $this->_sea_valve_row_data_validation_and_get_error_message($key, $row_data);
+                        if ($row_data_validation) {
+                            $has_error_class = true;
+                            $error_message_on_this_row .= "<li>" . $row_data_validation . "</li>";
+                            $got_error_table_data = true;
+                        }
+                    }
+
+                    if (count($headers) > $key) {
+                        $table_data_body_array[$row_key][] = array("has_error_class" => $has_error_class, "value" => $row_data);
+                    }
+                }
+
+                $error_message_on_this_row .= "</ol>";
+
+                //error messages for this row
+                if ($got_error_table_data) {
+                    $table_data_body_array[$row_key][] = array("has_error_text" => true, "value" => $error_message_on_this_row);
+                }
+            }
+        }
+
+        //return false if any error found on submitting file
+        if ($check_on_submit) {
+            return ($got_error_header || $got_error_table_data) ? false : true;
+        }
+
+        //add error header if there is any error in table body
+        if ($got_error_table_data) {
+            array_push($table_data_header_array, array("has_error_text" => true, "value" => app_lang("error")));
+        }
+
+        //add headers to table
+        $table_data .= "<tr>";
+        foreach ($table_data_header_array as $table_data_header) {
+            $error_class = get_array_value($table_data_header, "has_error_class") ? "error" : "";
+            $error_text = get_array_value($table_data_header, "has_error_text") ? "text-danger" : "";
+            $value = get_array_value($table_data_header, "value");
+            $table_data .= "<th class='$error_class $error_text'>" . $value . "</th>";
+        }
+        $table_data .= "</tr>";
+
+        //add body data to table
+        foreach ($table_data_body_array as $table_data_body_row) {
+            $table_data .= "<tr>";
+            $error_text = "";
+
+            foreach ($table_data_body_row as $table_data_body_row_data) {
+                $error_class = get_array_value($table_data_body_row_data, "has_error_class") ? "error" : "";
+                $error_text = get_array_value($table_data_body_row_data, "has_error_text") ? "text-danger" : "";
+                $value = get_array_value($table_data_body_row_data, "value");
+                $table_data .= "<td class='$error_class $error_text'>" . $value . "</td>";
+            }
+
+            if ($got_error_table_data && !$error_text) {
+                $table_data .= "<td></td>";
+            }
+
+            $table_data .= "</tr>";
+        }
+
+        //add error message for header
+        if ($error_message) {
+            $total_columns = count($table_data_header_array);
+            $table_data .= "<tr><td class='text-danger' colspan='$total_columns'><i data-feather='alert-triangle' class='icon-16'></i> " . $error_message . "</td></tr>";
+        }
+
+        $table_data .= "</table>";
+
+        echo json_encode(array("success" => true, 'table_data' => $table_data, 'got_error' => ($got_error_header || $got_error_table_data) ? true : false));
+    }
+
     function save_sea_valve() {
         // if (!$this->can_edit_clients()) {
         //     app_redirect("forbidden");
--- app/Views/clients/sea_valves/index.php
+++ app/Views/clients/sea_valves/index.php
@@ -2,6 +2,7 @@
     <div class="tab-title clearfix">
         <h4><?php echo app_lang('sea_valves'); ?></h4>
         <div class="title-button-group">
+            <?php echo modal_anchor(get_uri("clients/import_sea_valves_modal_form"), "<i data-feather='upload' class='icon-16'></i> " . app_lang('import_sea_valves'), array("class" => "btn btn-default", "title" => app_lang('import_sea_valves'), "data-post-client_id" => $client_id)); ?>
             <?php echo modal_anchor(get_uri("clients/sea_valve_modal_form"), "<i data-feather='plus-circle' class='icon-16'></i> " . app_lang('add_sea_valve'), array("class" => "btn btn-default", "title" => app_lang('add_sea_valve'), "data-post-client_id" => $client_id)); ?>
         </div>
     </div>
