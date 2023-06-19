b1b773c886876e7aef1ec17587876e54e7b14059
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-06-18 22:27:10 -0400

    Import clients

--- app/Views/clients/index.php
+++ app/Views/clients/index.php
@@ -5,6 +5,7 @@
             <div class="tab-title clearfix no-border">
                 <div class="title-button-group">
                     <?php if ($can_edit_clients) { ?>
+                        <?php echo modal_anchor(get_uri("clients/import_clients_modal_form"), "<i data-feather='upload' class='icon-16'></i> " . app_lang('import_vessels'), array("class" => "btn btn-default", "title" => app_lang('import_vessels'))); ?>
                         <?php echo modal_anchor(get_uri("clients/modal_form"), "<i data-feather='plus-circle' class='icon-16'></i> " . app_lang('add_vessel'), array("class" => "btn btn-default", "title" => app_lang('add_vessel'))); ?>
                     <?php } ?>
                 </div>
