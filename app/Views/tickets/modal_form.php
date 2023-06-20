aef50692c2cc4b1605baa96b02b960d115bf2cf3
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-06-20 16:25:44 -0400

    Fixed issues on tickets module

--- app/Views/tickets/modal_form.php
+++ app/Views/tickets/modal_form.php
@@ -30,7 +30,7 @@
             <?php } else if (!$model_info->creator_email) { ?>
                 <div class="form-group">
                     <div class="row">
-                        <label for="client_id" class=" col-md-3"><?php echo app_lang('client'); ?></label>
+                        <label for="client_id" class=" col-md-3"><?php echo app_lang('vessel'); ?></label>
                         <div class="col-md-9">
                             <?php
                             echo form_dropdown("client_id", $clients_dropdown, array($model_info->client_id), "class='select2 validate-hidden' id='client_id' data-rule-required='true', data-msg-required='" . app_lang('field_required') . "'");
