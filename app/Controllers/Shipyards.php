08712ab5471fd1734ed1a32f2b8241a4a10bc034
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-07-28 04:52:40 -0400

    Rework shipyards

--- app/Controllers/Shipyards.php
+++ app/Controllers/Shipyards.php
@@ -12,6 +12,13 @@ class Shipyards extends Security_Controller {
         return $this->country();
     }
 
+    private function can_edit_items() {
+        if ($this->login_user->is_admin) {
+            return true;
+        }
+        return false;
+    }
+
     function country($country_id = "") {
         $view_data["active_tab"] = "country";
         if (empty($country_id)) {
@@ -33,6 +40,7 @@ class Shipyards extends Security_Controller {
             $view_data["repair_list"] = $this->repair_list($companies);
             $view_data["new_build_list"] = $this->new_build_list($companies);
             $view_data["scrapping_list"] = $this->scrapping_list($companies);
+            $view_data["can_edit_items"] = $this->can_edit_items();
 
             return $this->template->rander("shipyards/view", $view_data);
         }
@@ -58,6 +66,8 @@ class Shipyards extends Security_Controller {
             $view_data["repair_list"] = $this->repair_list($companies);
             $view_data["new_build_list"] = $this->new_build_list($companies);
             $view_data["scrapping_list"] = $this->scrapping_list($companies);
+            $view_data["can_edit_items"] = $this->can_edit_items();
+
             return $this->template->rander("shipyards/view", $view_data);
         }
     }
@@ -82,11 +92,13 @@ class Shipyards extends Security_Controller {
             $view_data["repair_list"] = $this->repair_list($companies);
             $view_data["new_build_list"] = $this->new_build_list($companies);
             $view_data["scrapping_list"] = $this->scrapping_list($companies);
+            $view_data["can_edit_items"] = $this->can_edit_items();
+
             return $this->template->rander("shipyards/view", $view_data);
         }
     }
 
-    function modal_form($id = 0) {
+    function modal_view($id = 0) {
         $model_info = $this->Shipyards_model->get_details(array("id" => $id))->getRow();
         if (empty($model_info->id)) {
             show_404();
@@ -94,7 +106,75 @@ class Shipyards extends Security_Controller {
             $view_data["model_info"] = $model_info;
             $view_data["label_column"] = "col-md-3";
             $view_data["field_column"] = "col-md-9";
-            return $this->template->view("shipyards/modal_form", $view_data);
+            return $this->template->view("shipyards/modal_view", $view_data);
+        }
+    }
+
+    function modal_form() {
+        $id = $this->request->getPost("id");
+        $model_info = $this->Shipyards_model->get_one($id);
+
+        $view_data["model_info"] = $model_info;
+        $view_data["label_column"] = "col-md-3";
+        $view_data["field_column"] = "col-md-9";
+        $view_data["country_map"] = $this->Shipyards_model->get_country_map();
+        $view_data["countries_dropdown"] = $this->_get_countries_dropdown();
+        $view_data["regions_dropdown"] = $this->_get_regions_dropdown();
+        $view_data["sailing_areas_dropdown"] = $this->_get_sailing_areas_dropdown();
+        $view_data["services_dropdown"] = $this->_get_services_dropdown();
+        return $this->template->view("shipyards/modal_form", $view_data);
+    }
+
+    function save() {
+        if (!$this->can_edit_items()) {
+            app_redirect("forbidden");
+        }
+
+        $id = $this->request->getPost("id");
+        $this->validate_submitted_data(array(
+            "id" => "required",
+            "name" => "required",
+            "country_id" => "required",
+            "region_id" => "required",
+            "sailingarea_id" => "required",
+            "lat" => "required",
+            "lon" => "required",
+            "services" => "required"
+        ));
+
+        $data = array(
+            "id" => $this->request->getPost("id"),
+            "name" => $this->request->getPost("name"),
+            "description" => $this->request->getPost("description"),
+            "country_id" => strtoupper($this->request->getPost("country_id")),
+            "region_id" => $this->request->getPost("region_id"),
+            "sailingarea_id" => $this->request->getPost("sailingarea_id"),
+            "lat" => $this->request->getPost("lat"),
+            "lon" => $this->request->getPost("lon"),
+            "url" => $this->request->getPost("url"),
+            "services" => $this->request->getPost("services"),
+            "score" => $this->request->getPost("score"),
+            "maxLength" => $this->request->getPost("maxLength"),
+            "maxWidth" => $this->request->getPost("maxWidth"),
+            "maxDepth" => $this->request->getPost("maxDepth"),
+            "docksCount" => $this->request->getPost("docksCount"),
+            "cranesCount" => $this->request->getPost("cranesCount"),
+            "dock" => $this->request->getPost("dock"),
+            "email" => $this->request->getPost("email"),
+            "phone" => $this->request->getPost("phone"),
+            "fax" => $this->request->getPost("fax"),
+            "city" => $this->request->getPost("city"),
+            "street" => $this->request->getPost("street"),
+            "zip" => $this->request->getPost("zip"),
+            "only_new_build" => $this->request->getPost("only_new_build"),
+            "published" => $this->request->getPost("published")
+        );
+
+        $save_id = $this->Shipyards_model->ci_save($data, $id);
+        if ($save_id) {
+            echo json_encode(array("success" => true, 'message' => app_lang('record_saved')));
+        } else {
+            echo json_encode(array("success" => false, 'message' => app_lang('error_occurred')));
         }
     }
 
@@ -129,7 +209,7 @@ class Shipyards extends Security_Controller {
     }
 
     private function _make_row($data) {
-        $name = modal_anchor(get_uri("shipyards/modal_form/" . $data->id), $data->name, array("class" => "shipyard-text", "title" => $data->name . " " . app_lang("information"), "data-id" => '_' . $data->id));
+        $name = modal_anchor(get_uri("shipyards/modal_view/" . $data->id), $data->name, array("class" => "shipyard-text", "title" => $data->name . " " . app_lang("information"), "data-id" => '_' . $data->id));
 
         $services = '<div class="shipyard-row">';
         if (str_contains($data->services, "service-2")) {
@@ -139,7 +219,7 @@ class Shipyards extends Security_Controller {
             $services .= '<img src="/assets/images/shipyards/newbuilding.png" title="New Buildings" alt="New Buildings"/>';
         }
         if (str_contains($data->services, "service-3")) {
-            $services .= '<img src="/assets/images/shipyards/recycling.png" title="Recycling" alt="Recycling"/>';
+            $services .= '<img src="/assets/images/shipyards/recycling.png" title="Scrapping" alt="Scrapping"/>';
         }
         $services .= '</div>';
 
@@ -154,6 +234,42 @@ class Shipyards extends Security_Controller {
             $data->maxDepth,
             $data->phone
         );
+    }
+
+    private function _get_services_dropdown() {
+        $dropdown = [
+            [ "id" => "service-2", "text" => app_lang("repairs") ],
+            [ "id" => "service-1", "text" => app_lang("new_buildings") ],
+            [ "id" => "service-3", "text" => app_lang("scrapping") ]
+        ];
+        return json_encode($dropdown);
+    }
+
+    private function _get_sailing_areas_dropdown() {
+        $list = $this->Shipyards_model->get_sailing_areas();
+        $dropdown = array("" => "-");
+        foreach ($list as $item) {
+            $dropdown[$item->id] = $item->name;
+        }
+        return $dropdown;
+    }
+
+    private function _get_regions_dropdown() {
+        $list = $this->Shipyards_model->get_regions();
+        $dropdown = array("" => "-");
+        foreach ($list as $item) {
+            $dropdown[$item->id] = $item->name;
+        }
+        return $dropdown;
+    }
+
+    private function _get_countries_dropdown() {
+        $list = $this->Shipyards_model->get_countries();
+        $dropdown = array("" => "-");
+        foreach ($list as $item) {
+            $dropdown[$item->id] = $item->name;
+        }
+        return $dropdown;
     }
 
     function temp() {
---
+++ app/Views/shipyards/modal_view.php
@@ -0,0 +1,197 @@
+<div class="modal-body clearfix" style="height: 768px; position: relative; overflow: auto;">
+    <div class="container-fluid">
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("id"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->id;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("name"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->name;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("country"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->country;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("description"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <span><?php echo $model_info->description;?></span>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("services"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <?php
+                    $services = $model_info->services;
+                    $services = str_replace("service-1", app_lang("new_buildings"), $services);
+                    $services = str_replace("service-2", app_lang("repairs"), $services);
+                    $services = str_replace("service-3", app_lang("scrapping"), $services);
+                    $services = str_replace(",", ", ", $services);
+                ?>
+                <strong><?php echo $services;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("score"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->score;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("maxLength"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->maxLength;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("maxWidth"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->maxWidth;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("maxDepth"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->maxDepth;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("docksCount"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->docksCount;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("cranesCount"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->cranesCount;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("dock"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->dock;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("email"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->email;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("phone"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->phone;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("fax"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->fax;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("only_new_build"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->only_new_build == "1" ? app_lang("yes") : app_lang("no");?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("published"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->published == "1" ? app_lang("yes") : app_lang("no");?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("city"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->city;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("street"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->street;?></strong>
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <div class="<?php echo $label_column; ?>">
+                <span><?php echo app_lang("zip"); ?>:</span>
+            </div>
+            <div class="<?php echo $field_column; ?>">
+                <strong><?php echo $model_info->zip;?></strong>
+            </div>
+        </div>
+
+    </div>
+</div>
+<div class="modal-footer">
+    <?php
+        echo modal_anchor(get_uri("shipyards/modal_form"), "<i data-feather='edit-2' class='icon-16'></i> " . app_lang('edit'), array("class" => "btn btn-default", "data-post-id" => $model_info->id, "title" => app_lang('edit')));
+    ?>
+    <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
+</div>
