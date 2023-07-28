a07548097e8fc1b743a26a0fc3983b497a1cd59e
Curest0x <43941751+curest0x1021@users.noreply.github.com>
2023-07-28 09:19:17 -0400

    Add back button, update import files

--- app/Views/misc/view.php
+++ app/Views/misc/view.php
@@ -2,11 +2,12 @@
     <div class="container-fluid  full-width-button">
         <div class="row clients-view-button">
             <div class="col-md-12">
+                <a onclick="history.back()" style="cursor: pointer; font-size: 16px;"><i data-feather="arrow-left" class="icon-24"></i><?php echo app_lang("back"); ?></a>
                 <div class="page-title clearfix no-border no-border-top-radius no-bg">
                     <h1 class="pl0"><?php echo $main_info->item_description . " (" . $vessel->charter_name . ")"; ?></h1>
                 </div>
                 <ul id="misc-tabs" data-bs-toggle="ajax-tab" class="nav nav-tabs scrollable-tabs title" role="tablist">
-                    <li><a role="presentation" data-bs-toggle="tab" href="<?php echo_uri("misc/info_tab/" . $client_id . "/" . $main_id); ?>" data-bs-target="#misc-info"> <?php $main_info->item_description; ?></a></li>
+                    <li><a role="presentation" data-bs-toggle="tab" href="<?php echo_uri("misc/info_tab/" . $client_id . "/" . $main_id); ?>" data-bs-target="#misc-info"> <?php echo $main_info->item_description; ?></a></li>
                     <li><a role="presentation" data-bs-toggle="tab" href="<?php echo_uri("misc/loadtest_tab/" . $client_id . "/" . $main_id); ?>" data-bs-target="#misc-loadtest"> <?php echo app_lang('loadtest'); ?></a></li>
                     <li><a role="presentation" data-bs-toggle="tab" href="<?php echo_uri("misc/inspection_tab/" . $client_id . "/" . $main_id); ?>" data-bs-target="#misc-inspection"> <?php echo app_lang('visual_inspection'); ?></a></li>
                 </ul>
--- app/Views/shackles/view.php
+++ app/Views/shackles/view.php
@@ -2,6 +2,7 @@
     <div class="container-fluid  full-width-button">
         <div class="row clients-view-button">
             <div class="col-md-12">
+                <a onclick="history.back()" style="cursor: pointer; font-size: 16px;"><i data-feather="arrow-left" class="icon-24"></i><?php echo app_lang("back"); ?></a>
                 <div class="page-title clearfix no-border no-border-top-radius no-bg">
                     <h1 class="pl0"><?php echo $main_info->item_description . " (" . $vessel->charter_name . ")"; ?></h1>
                 </div>
