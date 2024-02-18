ec3b24a38e5466449ebf8ed08d979aee332a5862
curest0x1021 <phillip.mr0x@gmail.com>
2024-02-18 10:01:23 -0800

    update

---
+++ app/Views/projects/modal_add_yard.php
@@ -0,0 +1,120 @@
+<div class="modal-body clearfix">
+    <div class="container-fluid">
+        <div class="form-group">
+            <div class="row">
+                <label for="title" class=" col-md-3"><?php echo app_lang('title'); ?></label>
+                <div class=" col-md-9">
+                    <?php
+                    echo form_input(array(
+                        "id" => "title",
+                        "name" => "title",
+                        "value" => $model_info->title,
+                        "class" => "form-control",
+                        "placeholder" => app_lang('title'),
+                        "autofocus" => true,
+                        "data-rule-required" => true,
+                        "data-msg-required" => app_lang("field_required"),
+                    ));
+                    ?>
+                </div> 
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <label for="description" class=" col-md-3"><?php echo app_lang('description'); ?></label>
+                <div class=" col-md-9">
+                    <?php
+                    echo form_textarea(array(
+                        "id" => "description",
+                        "name" => "description",
+                        "value" => process_images_from_content($model_info->description, false),
+                        "class" => "form-control",
+                        "placeholder" => app_lang('description'),
+                        "style" => "height:150px;",
+                        "data-rich-text-editor" => true
+                    ));
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <label for="start_date" class=" col-md-3"><?php echo app_lang('start_date'); ?></label>
+                <div class=" col-md-9">
+                    <?php
+                    echo form_input(array(
+                        "id" => "start_date",
+                        "name" => "start_date",
+                        "value" => is_date_exists($model_info->start_date) ? $model_info->start_date : "",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('start_date'),
+                        "autocomplete" => "off"
+                    ));
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <label for="deadline" class=" col-md-3"><?php echo app_lang('deadline'); ?></label>
+                <div class=" col-md-9">
+                    <?php
+                    echo form_input(array(
+                        "id" => "deadline",
+                        "name" => "deadline",
+                        "value" => is_date_exists($model_info->deadline) ? $model_info->deadline : "",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('deadline'),
+                        "autocomplete" => "off"
+                    ));
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="price" class=" col-md-3"><?php echo app_lang('price'); ?></label>
+                <div class=" col-md-9">
+                    <?php
+                    echo form_input(array(
+                        "id" => "price",
+                        "name" => "price",
+                        "value" => $model_info->price ? to_decimal_format($model_info->price) : "",
+                        "class" => "form-control",
+                        "placeholder" => app_lang('price')
+                    ));
+                    ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="row">
+                <label for="project_labels" class=" col-md-3"><?php echo app_lang('labels'); ?></label>
+                <div class=" col-md-9">
+                    <?php
+                    echo form_input(array(
+                        "id" => "project_labels",
+                        "name" => "labels",
+                        "value" => $model_info->labels,
+                        "class" => "form-control",
+                        "placeholder" => app_lang('labels')
+                    ));
+                    ?>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </div>
+</div>
+
+<div class="modal-footer">
+
+    <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
+        <button type="button" id="save-and-continue-button" class="btn btn-info text-white"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save_and_continue'); ?></button>
+    <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
+</div>
