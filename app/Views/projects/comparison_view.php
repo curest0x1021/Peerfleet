9a6a872763f5c06ebd564d37a4345710d8f3c946
curest0x1021 <phillip.mr0x@gmail.com>
2024-02-18 11:37:12 -0800

    modal

--- app/Views/projects/comparison_view.php
+++ app/Views/projects/comparison_view.php
@@ -416,11 +416,14 @@
         </div>
         <div class="card" >
             <p>By selecting this yard, the quoted costs for this project. Data from any other yards will be hidden. This action is not reversible, but you can always edit the costs in execution phase.</p>
-            <div class="card" style="background-color:yellow;border:1px solid brown;" >
+            <div class="card" style="background-color:lightyellow;border:1px solid brown;" >
                 <ul>
-                <p>This action will move the project to the pre-Execution phase. This is the phase between yard selection and the actual yard visit(Execution)
+                <p>
+                    This action will move the project to the pre-Execution phase. This is the phase between yard selection and the actual yard visit(Execution)
                 </p>
-                <p>This action will move the project to the pre-Execution phase. This is the phase between yard selection and the actual yard visit(Execution)
+                <p>
+                    Please note that the Deviation cost loss of earnings, Bunker cost and other additional expenditures at yard are not transfered to the cost.
+                    In order to include these in the project's total cost, you should add these on the Owner's supply page.
                 </p>
                 </ul>
             </div>
