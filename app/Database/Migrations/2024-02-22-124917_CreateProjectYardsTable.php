25d3570bf56247254228d32050816ad5095d338c
curest0x1021 <phillip.mr0x@gmail.com>
2024-02-22 04:53:28 -0800

    import excel file

---
+++ app/Database/Migrations/2024-02-22-124917_CreateProjectYardsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProjectYardsTable extends Migration
+{
+    public function up()
+    {
+        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'shipyard_id'=>[
+                'type'=>'INT',
+                'null'=>true
+            ],
+            'project_id'=>[
+                'type'=>'INT',
+                'null'=>true
+            ],
+            'title'=>[
+                'type'=>'VARCHAR',
+                'null'=>true,
+                'constraint'=>300
+            ],
+            'work_orders'=>[
+                'type'=>'VARCHAR',
+                'null'=>true,
+                'constraint'=>500
+            ],
+            'currencies'=>[
+                'type'=>'VARCHAR',
+                'null'=>true,
+                'constraint'=>500
+            ],
+            // Add other fields here...
+        ]);
+    
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('project_yards');
+    }
+
+    public function down()
+    {
+        //
+        $this->forge->drop('project_yards');
+    }
+}
