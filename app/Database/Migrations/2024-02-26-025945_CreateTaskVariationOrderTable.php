8ee30fdaa2f062a3bc38b1719a34673a0f4a0030
curest0x1021 <phillip.mr0x@gmail.com>
2024-02-25 22:47:18 -0800

    added variation order

---
+++ app/Database/Migrations/2024-02-26-025945_CreateTaskVariationOrderTable.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTaskVariationOrderTable extends Migration
+{
+    public function up()
+    {
+        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            "task_id"=>[
+                'type'=>"INT",
+                'null'=>true
+            ],
+            "name"=>[
+                'type'=>"VARCHAR",
+                'constraint' =>200,
+                'null'=>true
+            ],
+            "cost"=>[
+                'type'=>"DOUBLE",
+                'null'=>true
+            ],
+            "start_date"=>[
+                'type'=>"DATE",
+                'null'=>true
+            ],
+            "finish_date"=>[
+                'type'=>"DATE",
+                'null'=>true
+            ],
+            "notes"=>[
+                'type'=>"VARCHAR",
+                "constraint"=>1000,
+                'null'=>true
+            ],
+            // Add other fields here...
+        ]);
+    
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('task_variation_orders');
+    }
+
+    public function down()
+    {
+        //
+        $this->forge->drop('task_variation_orders');
+    }
+}
