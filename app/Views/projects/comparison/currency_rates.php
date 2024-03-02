edfd6bef94c33dc9dedafba891e595171ea5a315
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-02 05:19:09 -0800

    currency rates table bug

--- app/Views/projects/comparison/currency_rates.php
+++ app/Views/projects/comparison/currency_rates.php
@@ -9,7 +9,7 @@
             <p>It is crucial to specify the accurate exchange rates that will be utilized to convert the cost provided from the specific yard.</p>
         </div>
     </div>
-    <table class="table table-hover tabel-bordered">
+    <table class="table table-bordered">
         <thead>
             <tr>
             <th>From</th>
