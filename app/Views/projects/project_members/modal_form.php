5eda374ac1647c2a76df360a36542ef96cf188b1
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-04 12:39:29 -0800

    add project issue

--- app/Views/projects/project_members/modal_form.php
+++ app/Views/projects/project_members/modal_form.php
@@ -29,7 +29,8 @@
     <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
 
     <?php if ($view_type == "from_project_modal") { ?>
-        <button type="button" id="next-button" class="btn btn-info text-white"><span data-feather="arrow-right-circle" class="icon-16"></span> <?php echo app_lang('next'); ?></button>
+        <!-- <button type="button" id="next-button" class="btn btn-info text-white"><span data-feather="arrow-right-circle" class="icon-16"></span> <?php //echo app_lang('next'); ?></button> -->
+        <button type="button" id="next-button" class="btn btn-primary text-white"><span data-feather="save" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
         <button type="button" id="save-and-continue-button" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save_and_continue'); ?></button>
     <?php } else { ?>
         <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
@@ -107,7 +108,8 @@
         });
 
         $("#next-button").click(function () {
-            showAddMultipleTaskModal();
+            // showAddMultipleTaskModal();
+            $(this).trigger("submit");
         });
 
         function showAddMultipleTaskModal() {
