73c4791e5de1cdc95c32ddf79707038714056a6d
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-05 08:00:02 -0800

    project overview widget height

--- app/Views/projects/project_members/modal_form.php
+++ app/Views/projects/project_members/modal_form.php
@@ -31,7 +31,7 @@
     <?php if ($view_type == "from_project_modal") { ?>
         <!-- <button type="button" id="next-button" class="btn btn-info text-white"><span data-feather="arrow-right-circle" class="icon-16"></span> <?php //echo app_lang('next'); ?></button> -->
         <button type="button" id="save-and-continue-button" class="btn btn-info text-white"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save_and_continue'); ?></button>
-        <button type="button" id="next-button" class="btn btn-primary text-white"><span data-feather="save" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
+        <button type="button" id="next-button" class="btn btn-primary text-white"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
         
     <?php } else { ?>
         <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
