f007704c1ea8e55fd7e218eb28ec34c35a92e64f
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-05 06:31:02 -0800

    export item ui

--- app/Views/projects/comparison/modal_select_yard.php
+++ app/Views/projects/comparison/modal_select_yard.php
@@ -1,7 +1,7 @@
 <!-- The Modal -->
 
 <div class="modal-body">
-    <h5>Selecting yard - <?php echo $shipyard_info->name;?></h5>
+    <h5>Selecting candidate - <?php echo $shipyard_info->name;?></h5>
     <!-- <p><i data-feather="info" class="icon-16"></i> Learn more about selecting yards.</p> -->
 <div class="card" >
     <div class="card-body" >
@@ -9,12 +9,12 @@
         <div style="border-radius:10px;background-color:lightyellow;border:1px solid brown;" >
             <ul>
             <p>
-                This action will move the project to the only Execution Phase. This is the phase between yard selection and the actual yard visit(Execution)
+                This action will move the project to the only Execution Phase. The status of your project will automatically change to the Execution phase.
             </p>
-            <!-- <p>
-                Please note that the Deviation, cost loss of earnings, Bunker cost and other additional expenditures at yard are not transfered to the cost.
-                In order to include these in the project's total cost, you should add these on the Owner's supply page.
-            </p> -->
+            <p>
+                Please note that the Deviation cost, Loss of earnings, Bunker cost and other additional expenditures at yard are not transfered to the cost overview.
+                In order to include these in the project's total cost overview, you should add these as Owner's supply to a designated task.
+            </p>
             </ul>
         </div>
     </div>
