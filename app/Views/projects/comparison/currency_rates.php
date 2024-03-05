4ab869b21172ce922e1406d5635dd963917ae294
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-05 12:48:02 -0800

    currency rates

--- app/Views/projects/comparison/currency_rates.php
+++ app/Views/projects/comparison/currency_rates.php
@@ -1,5 +1,5 @@
 <div class="container" >
-    <h1>Currencies</h1>
+    <h3>Currencies</h3>
     <?php
     $currencies=array();
     foreach ($allCostItemCurrencies as $oneCurrency) {
@@ -15,41 +15,51 @@
             <p>It is crucial to specify the accurate exchange rates that will be utilized to convert the cost provided from the specific yard.</p>
         </div>
     </div> -->
-    <div style="min-height:30vh;" >
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-            <th>From</th>
-            <th>To</th>
-            <th>Rate</th>
-            <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            foreach ($allCurrencyRates as $oneRate) {
-            ?>
-            <tr>
-                <td>
-                    <p> <?php echo $oneRate->from;?></p>
-                </td>
-                <td><?php echo $oneRate->to;?></td>
-                <td><?php echo $oneRate->rate;?></td>
-                <td>
-                    <?php echo modal_anchor(get_uri("projects/modal_edit_currency_rate/".$oneRate->id),'EDIT',array()); ?>
-                     | <a href="#" >DELETE</a>
-                </td>
-            </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
-    </div>
-    <div class="row" >
-        <div class="col-md-3" >
-            <!-- <button class="btn btn-primary" >Add exchange rate</button> -->
-            <?php echo modal_anchor(get_uri("projects/modal_add_currency_rate/").$project_id,'<button class="btn btn-primary" >Add exchange rate</button>',array()); ?>
+    <div class="card" >
+        <div class="card-body" >
+            <p><i data-feather="alert-circle" class="icon-16" ></i>Please enter the exchange rates for the currencies used in quotations. peerfleet will use these exchange rates to convert the quotes into your set project currency.</p>
+            <div style="min-height:30vh;" >
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th>From</th>
+                        <th>To</th>
+                        <th>Rate</th>
+                        <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($allCurrencyRates as $oneRate) {
+                        ?>
+                        <tr>
+                            <td>
+                                <p> <?php echo $oneRate->from;?></p>
+                            </td>
+                            <td><?php echo $oneRate->to;?></td>
+                            <td><?php echo $oneRate->rate;?></td>
+                            <td>
+                                <?php echo modal_anchor(get_uri("projects/modal_edit_currency_rate/".$oneRate->id),'EDIT',array()); ?>
+                                | <?php echo js_anchor("DELETE", array('title' => app_lang('delete_project'),"data-bs-toggle"=>"modal", "data-bs-target"=>"#confirmationModal", "class" => "delete", "data-id" => $oneRate->id, "data-action-url" => get_uri("projects/delete"), "data-action" => "delete-confirmation"));?>
+                            </td>
+                        </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="d-flex" >
+                        <a class="btn btn-ddefault" href="<?php echo get_uri("projects/view/").$project_id;?>" >Cancel</a>
+                    <!-- <button class="btn btn-primary" >Add exchange rate</button> -->
+                    <div class="flex-grow-1" ></div>
+                    <?php echo modal_anchor(get_uri("projects/modal_add_currency_rate/").$project_id,'<button class="btn btn-primary" >Add exchange rate</button>',array()); ?>
+            </div>
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function(){
+
+    })
+</script>
