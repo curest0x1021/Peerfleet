b0f172c87d893c5d4d94a0a2a9739292a008c14c
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-18 14:45:51 +0100

    quote overview added-fields

--- app/Database/Migrations/2024-03-18-134251_AddFieldsToProjectYardsTable.php
+++ app/Database/Migrations/2024-03-18-134251_AddFieldsToProjectYardsTable.php
@@ -9,10 +9,28 @@ class AddFieldsToProjectYardsTable extends Migration
     public function up()
     {
         //
+        $this->forge->addColumn('project_yards', [
+            'penalty_price' => [
+                'type' => 'INT',
+                "null"=>true,
+                'constraint'=>10
+            ],
+            'penalty_limit' => [
+                'type' => 'INT',
+                "null"=>true,
+                'constraint'=>3
+            ],
+            'payment_terms' => [
+                'type' => 'INT',
+                "null"=>true,
+                'constraint'=>3
+            ],
+        ]);
     }
 
     public function down()
     {
         //
+        $this->forge->dropColumn('clients', 'project_yards');
     }
 }
