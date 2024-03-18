91a116113431334eaad5b023e839517640680ab3
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-18 18:04:26 +0100

    clients tab

--- app/Views/clients/view.php
+++ app/Views/clients/view.php
@@ -63,7 +63,8 @@
                 <ul id="vessel-tabs" data-bs-toggle="ajax-tab" class="nav nav-tabs scrollable-tabs" role="tablist">
                     <li><a  role="presentation" data-bs-toggle="tab" href="<?php echo_uri("clients/company_info_tab/" . $client_info->id); ?>" data-bs-target="#client-info"> <?php echo app_lang('vessel_info'); ?></a></li>
                     <li><a  role="presentation" data-bs-toggle="tab" href="<?php echo_uri("clients/contacts/" . $client_info->id); ?>" data-bs-target="#client-contacts"> <?php echo app_lang('communication'); ?></a></li>
-
+                    <li><a  role="presentation" data-bs-toggle="tab" href="<?php echo_uri("clients/contacts/" . $client_info->id); ?>" data-bs-target="#client-contacts"> Dimensions & Capacities</a></li>
+                    <li><a  role="presentation" data-bs-toggle="tab" href="<?php echo_uri("clients/contacts/" . $client_info->id); ?>" data-bs-target="#client-contacts"> Propulsion</a></li>
                     <?php if ($show_project_info) { ?>
                         <li><a  role="presentation" data-bs-toggle="tab" href="<?php echo_uri("clients/projects/" . $client_info->id); ?>" data-bs-target="#client-projects"><?php echo app_lang('projects'); ?></a></li>
                     <?php } ?>
--- app/Views/projects/export_pdf/pages/first_page.php
+++ app/Views/projects/export_pdf/pages/first_page.php
@@ -1,5 +1,5 @@
 <?php echo view('projects/export_pdf/layouts/header.php');?>
-
+<img style="width:80%;height:auto;" src="<?php echo encode_img_base64(get_setting("profile_image_path")."/".unserialize($client_info->image)["file_name"]);?>" alt="logo"/>
 <h2><?php echo $client_info->charter_name;?> - <?php echo $project_info->title;?></h2>
 <h3>Start Date : <?php echo $project_info->start_date;?></h3>
 <h3>Deadline : <?php echo $project_info->deadline;?></h3>
@@ -8,3 +8,19 @@ $label_data=explode("--::--",$project_info->labels_list);
  echo $label_data[1];
  ?></h4>
 <div style="page-break-before: always;"></div>
+<?php echo view('projects/export_pdf/layouts/header.php');?>
+<h1>Vessel Information</h1>
+<h2>General Information</h2>
+<h2>Dimensions and Capacities</h2>
+<div style="page-break-before: always;"></div>
+<?php echo view('projects/export_pdf/layouts/header.php');?>
+<h2>Propulsion</h2>
+<div style="page-break-before: always;"></div>
+<?php echo view('projects/export_pdf/layouts/header.php');?>
+<h1>Side Thruster</h1>
+<div style="page-break-before: always;"></div>
+<?php echo view('projects/export_pdf/layouts/header.php');?>
+<h3>Project Description</h3>
+<div><?php echo $project_info->description;?></div>
+<h3>Team Members</h3>
+<div><?php echo $project_info->description;?></div>
