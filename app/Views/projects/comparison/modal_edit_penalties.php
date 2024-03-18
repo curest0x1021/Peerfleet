1119cb26cbbc77e7e6a63f145b5a2b6c48d28e85
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-18 14:43:49 +0100

    quote overview

--- app/Views/projects/comparison/modal_edit_penalties.php
+++ app/Views/projects/comparison/modal_edit_penalties.php
@@ -1,17 +1,19 @@
-<div class="modal-body">
+<div class="modal-body" id="panel-edit-penalties">
     <h3>Set Penalties of <?php echo $shipyard_info->title;?></h3>
+    <?php echo form_open(get_uri("projects/edit_penalties"), array("id" => "task-form", "class" => "general-form", "role" => "form")); ?>
+    <?php echo form_close();?>
     <div class="row" >
         <div class="col-md-6" >
             <div class="form-groud" >
                 <div class="input-group mb-3">
-                    <input type="number" class="form-control" value="0.00">
+                    <input type="number" class="form-control input-penalty-price" value="0.00">
                     <span class="input-group-text" ><?php echo $project_info->currency?$project_info->currency:"USD"; ?></span>
                 </div>
             </div>
             <div class="form-group" >
                 <label>Penalty Limitations</label>
                 <div class="input-group">
-                    <input type="number" max="100" min="0" class="form-control input-penality-limitation" value="0.00">
+                    <input type="number" max="100" min="0" class="form-control input-penalty-limitation" value="0.00">
                     <span class="input-group-text" >%</span>
                 </div>
             </div>
@@ -27,7 +29,25 @@
 <script>
     $(document).ready(function(){
         $(".save-penalties-<?php echo $shipyard_info->id;?>").on("click",function(){
-            console.log($(this))
+            var penalty_price=$(".input-penalty-limitation")[0].value;
+            var penalty_limit=$(".input-penalty-price")[0].value;
+            var rise_csrf_token = $('[name="rise_csrf_token"]').val();
+            $.ajax({
+                url:'<?php echo get_uri("projects/save_penalty");?>',
+                method:"POST",
+                data:{
+                    price:penalty_price,
+                    limit:penalty_limit,
+                    rise_csrf_token:rise_csrf_token,
+                    shipyard_id:<?php echo $shipyard_info->id;?>
+                },
+                success:function(response){
+                    console.log(response)
+                }
+            })
         })
+    })
+    window.panel_edit_penalties=$("#panel-edit-penalties").appForm({
+        closeModalOnSuccess:false
     })
 </script>
