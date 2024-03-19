19dd8408e6803b9c4d39b33cb567938d041c7664
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-19 19:11:06 +0100

    pdf exporting

--- app/Views/projects/export_pdf/pages/first_page.php
+++ app/Views/projects/export_pdf/pages/first_page.php
@@ -1,6 +1,6 @@
 <?php echo view('projects/export_pdf/layouts/header.php');?>
 <?php if(unserialize($client_info->image)){?>
-<img style="width:80%;height:auto;" src="<?php echo encode_img_base64(get_setting("profile_image_path")."/".unserialize($client_info->image)["file_name"]);?>" alt="logo"/>
+    <img style="width:80%;height:auto;" src="<?php echo encode_img_base64(get_setting("profile_image_path")."/".unserialize($client_info->image)["file_name"]);?>" alt="logo"/>
 <?php } ?>
 <h2><?php echo $client_info->charter_name;?> - <?php echo $project_info->title;?></h2>
 <h3>Start Date : <?php echo $project_info->start_date;?></h3>
@@ -29,38 +29,69 @@ if(array_key_exists(1,$label_data)) echo $label_data[1];
 <h4><?php if($client_info->hull_design) echo "Hull design : ".$client_info->hull_design; ?></h4>
 <div style="page-break-before: always;"></div>
 <?php echo view('projects/export_pdf/layouts/header.php');?>
-<div style="column-count:2;column-fill: balance;" >
+
 <h2>Propulsion</h2>
-<h3>Main engine</h3>
-<p><?php if($client_info->main_engine_maker) echo "Maker : ".$client_info->main_engine_maker; ?></p>
-<p><?php if($client_info->main_engine_model) echo "Model : ".$client_info->main_engine_model; ?></p>
-<p><?php if($client_info->main_engine_continuous_output) echo "Continuous output: ".$client_info->main_engine_continuous_output; ?></p>
-<p><?php if($client_info->main_engine_bore) echo "Bore : ".$client_info->main_engine_bore; ?></p>
-<p><?php if($client_info->main_engine_stroke) echo "Stroke : ".$client_info->main_engine_stroke." m"; ?></p>
-<p><?php if($client_info->main_engine_serial_number) echo "Serial number : ".$client_info->main_engine_serial_number." m"; ?></p>
-<p><?php if($client_info->main_engine_quantity) echo "Quantity : ".$client_info->main_engine_quantity; ?></p>
-<h3>Auxiliary engine</h3>
-<p><?php if($client_info->auxiliary_engine_maker) echo "Maker : ".$client_info->auxiliary_engine_maker; ?></p>
-<p><?php if($client_info->auxiliary_engine_model) echo "Model : ".$client_info->auxiliary_engine_model; ?></p>
-<p><?php if($client_info->auxiliary_engine_output) echo "Output : ".$client_info->auxiliary_engine_output; ?></p>
-<p><?php if($client_info->auxiliary_engine_serial_number) echo "Serial number : ".$client_info->auxiliary_engine_serial_number." m"; ?></p>
-<p><?php if($client_info->auxiliary_engine_quantity) echo "Quantity : ".$client_info->auxiliary_engine_quantity; ?></p>
-<h3>Emergency generator</h3>
-<p><?php if($client_info->emergency_generator_maker) echo "Maker : ".$client_info->emergency_generator_maker; ?></p>
-<p><?php if($client_info->emergency_generator_model) echo "Model : ".$client_info->emergency_generator_model; ?></p>
-<p><?php if($client_info->emergency_generator_output) echo "Output : ".$client_info->emergency_generator_output; ?></p>
-<p><?php if($client_info->emergency_generator_serial_number) echo "Serial number : ".$client_info->emergency_generator_serial_number." m"; ?></p>
-<p><?php if($client_info->emergency_generator_quantity) echo "Quantity : ".$client_info->emergency_generator_quantity; ?></p>
-<h3>Shaft generator</h3>
-<p><?php if($client_info->shaft_generator_maker) echo "Maker : ".$client_info->shaft_generator_maker; ?></p>
-<p><?php if($client_info->shaft_generator_model) echo "Model : ".$client_info->shaft_generator_model; ?></p>
-<p><?php if($client_info->shaft_generator_output) echo "Output : ".$client_info->shaft_generator_output; ?></p>
-<p><?php if($client_info->shaft_generator_serial_number) echo "Serial number : ".$client_info->shaft_generator_serial_number." m"; ?></p>
-<p><?php if($client_info->shaft_generator_quantity) echo "Quantity : ".$client_info->shaft_generator_quantity; ?></p>
+<div style="" >
+<table style="width:100%;" >
+    <tbody>
+        <tr>
+            <td style="width:50%;" >
+                <h3>Main engine</h3>
+                <p><?php if($client_info->main_engine_maker) echo "Maker : ".$client_info->main_engine_maker; ?></p>
+                <p><?php if($client_info->main_engine_model) echo "Model : ".$client_info->main_engine_model; ?></p>
+                <p><?php if($client_info->main_engine_continuous_output) echo "Continuous output: ".$client_info->main_engine_continuous_output; ?></p>
+                <p><?php if($client_info->main_engine_bore) echo "Bore : ".$client_info->main_engine_bore; ?></p>
+                <p><?php if($client_info->main_engine_stroke) echo "Stroke : ".$client_info->main_engine_stroke." m"; ?></p>
+                <p><?php if($client_info->main_engine_serial_number) echo "Serial number : ".$client_info->main_engine_serial_number." m"; ?></p>
+                <p><?php if($client_info->main_engine_quantity) echo "Quantity : ".$client_info->main_engine_quantity; ?></p>
+                <h3>Auxiliary engine</h3>
+                <p><?php if($client_info->auxiliary_engine_maker) echo "Maker : ".$client_info->auxiliary_engine_maker; ?></p>
+                <p><?php if($client_info->auxiliary_engine_model) echo "Model : ".$client_info->auxiliary_engine_model; ?></p>
+                <p><?php if($client_info->auxiliary_engine_output) echo "Output : ".$client_info->auxiliary_engine_output; ?></p>
+                <p><?php if($client_info->auxiliary_engine_serial_number) echo "Serial number : ".$client_info->auxiliary_engine_serial_number." m"; ?></p>
+                <p><?php if($client_info->auxiliary_engine_quantity) echo "Quantity : ".$client_info->auxiliary_engine_quantity; ?></p>
+                <h3>Emergency generator</h3>
+                <p><?php if($client_info->emergency_generator_maker) echo "Maker : ".$client_info->emergency_generator_maker; ?></p>
+                <p><?php if($client_info->emergency_generator_model) echo "Model : ".$client_info->emergency_generator_model; ?></p>
+                <p><?php if($client_info->emergency_generator_output) echo "Output : ".$client_info->emergency_generator_output; ?></p>
+                <p><?php if($client_info->emergency_generator_serial_number) echo "Serial number : ".$client_info->emergency_generator_serial_number." m"; ?></p>
+                <p><?php if($client_info->emergency_generator_quantity) echo "Quantity : ".$client_info->emergency_generator_quantity; ?></p>
+            </td>
+            <td>
+                <h3>Shaft generator</h3>
+                <p><?php if($client_info->shaft_generator_maker) echo "Maker : ".$client_info->shaft_generator_maker; ?></p>
+                <p><?php if($client_info->shaft_generator_model) echo "Model : ".$client_info->shaft_generator_model; ?></p>
+                <p><?php if($client_info->shaft_generator_output) echo "Output : ".$client_info->shaft_generator_output; ?></p>
+                <p><?php if($client_info->shaft_generator_serial_number) echo "Serial number : ".$client_info->shaft_generator_serial_number." m"; ?></p>
+                <p><?php if($client_info->shaft_generator_quantity) echo "Quantity : ".$client_info->shaft_generator_quantity; ?></p>
+                <h3>Propeller</h3>
+                <p><?php if($client_info->propeller_maker) echo "Maker : ".$client_info->propeller_maker; ?></p>
+                <p><?php if($client_info->propeller_type) echo "Type : ".$client_info->propeller_type; ?></p>
+                <p><?php if($client_info->propeller_number_of_blades) echo "Number of blades : ".$client_info->propeller_number_of_blades; ?></p>
+                <p><?php if($client_info->propeller_diameter) echo "Diameter : ".$client_info->propeller_diameter." m"; ?></p>
+                <p><?php if($client_info->propeller_pitch) echo "Pitch : ".$client_info->propeller_pitch; ?></p>
+                <p><?php if($client_info->propeller_material) echo "Material : ".$client_info->propeller_material; ?></p>
+                <p><?php if($client_info->propeller_weight) echo "Weight : ".$client_info->propeller_weight; ?></p>
+                <p><?php if($client_info->propeller_output) echo "Output : ".$client_info->propeller_output; ?></p>
+                <p><?php if($client_info->propeller_quantity) echo "Quantity : ".$client_info->propeller_quantity; ?></p>
+            </td>
+        </tr>
+    </tbody>
+</table>
+    
+    
 </div>
 <div style="page-break-before: always;"></div>
 <?php echo view('projects/export_pdf/layouts/header.php');?>
-<h1>Side Thruster</h1>
+<h2>Side Thruster</h2>
+<p><?php if($client_info->bow_thruster_number) echo "Number of bow thrusters : ".$client_info->bow_thruster_number; ?></p>
+<p><?php if($client_info->bow_thruster_maker) echo "Maker of bow thrusters : ".$client_info->bow_thruster_maker; ?></p>
+<p><?php if($client_info->bow_thruster_type) echo "Type of bow thrusters : ".$client_info->bow_thruster_type; ?></p>
+<p><?php if($client_info->bow_thruster_power) echo "Power of bow thrusters : ".$client_info->bow_thruster_power; ?></p>
+<p><?php if($client_info->stern_thruster_number) echo "Number of stern thrusters : ".$client_info->stern_thruster_number; ?></p>
+<p><?php if($client_info->stern_thruster_maker) echo "Maker of stern thrusters : ".$client_info->stern_thruster_maker; ?></p>
+<p><?php if($client_info->stern_thruster_type) echo "Type of stern thrusters : ".$client_info->stern_thruster_type; ?></p>
+<p><?php if($client_info->stern_thruster_power) echo "Power of stern thrusters : ".$client_info->stern_thruster_power; ?></p>
 <div style="page-break-before: always;"></div>
 <?php echo view('projects/export_pdf/layouts/header.php');?>
 <h3>Project Description</h3>
