9a77f1ca2ef320954448ac740740ceb9d61e8879
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-19 04:09:59 +0100

    vessels

---
+++ app/Views/clients/tabs/dimensions.php
@@ -0,0 +1,41 @@
+<div class="card" >
+    <div class="card-body">
+        <div class="row" >
+            <div class="col-md-6" >
+                <h4>
+                    Gross Tonnage : <?php echo $client_info->gross_tonnage;?>
+                </h4>
+                <h4>
+                    Net Tonnage : <?php echo $client_info->net_tonnage;?>
+                </h4>
+                <h4>
+                    Lightweight : <?php echo $client_info->lightweight;?>
+                </h4>
+                <h4>
+                    Length over all : <?php echo $client_info->length_over_all;?>
+                </h4>
+                <h4>
+                    Length between perpendiculars : <?php echo $client_info->length_between_perpendiculars;?>
+                </h4>
+                <h4>
+                    Length of waterline : <?php echo $client_info->length_of_waterline;?>
+                </h4>
+                <h4>
+                    BEAM/Bredth Moulded : <?php echo $client_info->breadth_moulded;?>
+                </h4>
+                <h4>
+                    Depth Moulded : <?php echo $client_info->depth_moulded;?>
+                </h4>
+                <h4>
+                    Draft/Draught Design : <?php echo $client_info->draught_design;?>
+                </h4>
+                <h4>
+                    Draught scantling : <?php echo $client_info->draught_scantling;?>
+                </h4>
+                <h4>
+                    Hull design : <?php echo $client_info->hull_design;?>
+                </h4>
+            </div>
+        </div>
+    </div>
+</div>
