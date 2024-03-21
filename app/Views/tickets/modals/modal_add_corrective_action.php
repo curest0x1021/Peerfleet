39ca0303b0d31b41301baad982cf7a1e2b754d83
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-21 10:17:21 +0100

    add action modal

---
+++ app/Views/tickets/modals/modal_add_corrective_action.php
@@ -0,0 +1,11 @@
+<div class="modal-body clearfix" >
+
+</div>
+<div class="modal-footer" >
+    <button type="button" class="btn btn-default cancel-upload" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
+</div>
+<script>
+    $(document).ready(function(){
+        
+    })
+</script>
--- app/Views/tickets/tabs/actions.php
+++ app/Views/tickets/tabs/actions.php
@@ -1,8 +1,8 @@
 <div class="card" >
-    <div class="card-header" >
-
-    </div>
     <div class="card-body" >
-
+        <h4>Actions</h4>
+        <?php echo modal_anchor(get_uri("tickets/modal_add_corrective_action/".$ticket_id),"<button class='btn btn-default btn-lg mb-1 w-100' ><i data-feather='plus' class='icon-16' ></i></button>",array());?>
+        <?php echo modal_anchor(get_uri("tickets/modal_add_corrective_action/".$ticket_id),"<button class='btn btn-default btn-lg mb-1 w-100' ><i data-feather='plus' class='icon-16' ></i></button>",array());?>
+        <?php echo modal_anchor(get_uri("tickets/modal_add_corrective_action/".$ticket_id),"<button class='btn btn-default btn-lg mb-1 w-100' ><i data-feather='plus' class='icon-16' ></i></button>",array());?>
     </div>
 </div>
