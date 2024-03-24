b8615d228bd4d6996b523246a784e8abdfec2cc0
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-24 20:41:56 +0100

    email delete

--- plugins/Mailbox/Views/mailbox/email_sub_title.php
+++
@@ -1,4 +0,0 @@
-<div class="bg-white p15 pl0">
-    <span class="text-off"><?php echo app_lang("created") . ": "; ?></span>
-    <?php echo format_to_relative_time($email_info->created_at); ?> 
-</div>
