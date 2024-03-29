15769f25215dd63782b8c8b9ea895f2d6f66db00
curest0x1021 <phillip.mr0x@gmail.com>
2024-03-29 18:02:50 +0100

    add return

--- app/Views/tickets/modals/modal_add_schedule.php
+++ app/Views/tickets/modals/modal_add_schedule.php
@@ -1,4 +1,9 @@
 <div class="modal-body clearfix" >
+    <div id="link-of-new-view" class="hide">
+        <?php
+        echo modal_anchor(get_uri("tasks/view"), "", array("data-modal-lg" => "1"));
+        ?>
+    </div>
     <div class="form-group" >
         <label for="schedule_port" >Port : </label>
         <input 
@@ -49,12 +54,19 @@
     </div>
 </div>
 <div class="modal-footer" >
-    <button class="btn btn-default" data-bs-dismiss="modal" ><i data-feather="x" class="icon-16" ></i>Close</button>
-    <button class="btn btn-primary" data-bs-dismiss="modal" ><i data-feather="check-circle" class="icon-16" ></i>Save</button>
+    <button class="btn btn-default btn-cancel-schedule "  ><i data-feather="x" class="icon-16" ></i>Close</button>
+    <button class="btn btn-primary btn-save-schedule"  ><i data-feather="check-circle" class="icon-16" ></i>Save</button>
 </div>
 <script>
     $(document).ready(function(){
         setDatePicker("#schedule_eta");
         setDatePicker("#schedule_etd");
+        $(".btn-cancel-schedule").on("click",function(){
+            var $newViewLink = $("#link-of-new-view").find("a");
+            $newViewLink.attr("data-action-url", "<?php echo get_uri("tickets/modal_add_corrective_action/".$ticket_id); ?>");
+            // $taskViewLink.attr("data-title", taskShowText + " #" + JSON.parse(response).saved_id);
+            $newViewLink.attr("data-post-id", <?php echo $ticket_id;?>);
+            $newViewLink.trigger("click");
+        })
     })
 </script>
