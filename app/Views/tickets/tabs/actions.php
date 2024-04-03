9dca275b7036469485caf96784205c4789e2abf0
curest0x1021 <phillip.mr0x@gmail.com>
2024-04-03 23:08:35 +0200

    ticket action

--- app/Views/tickets/tabs/actions.php
+++ app/Views/tickets/tabs/actions.php
@@ -7,9 +7,9 @@
                 <div class="d-flex align-items-center justify-content-around" >
                     <p><?php echo $action->corrective_action;?></p>
                     <div class="flex-grow-1" ></div>
-                    <span class="badge bg-secondary" style="margin-left:10px;" ><i data-feather="tool" class="icon-16" ></i></span>
-                    <span class="badge bg-secondary" style="margin-left:10px;" ><i data-feather="shopping-cart" class="icon-16" ></i></span>
-                    <span class="badge bg-secondary" style="margin-left:10px;" ><i data-feather="calendar" class="icon-16" ></i></span>
+                    <span class="badge <?php if($action->task_title){?>bg-secondary<?php }?>" style="margin-left:10px;" ><i data-feather="tool" class="icon-16" ></i></span>
+                    <span class="badge <?php if($action->requisition_title){?>bg-secondary<?php }?>" style="margin-left:10px;" ><i data-feather="shopping-cart" class="icon-16" ></i></span>
+                    <span class="badge <?php if($action->schedule_port){?>bg-secondary<?php }?>" style="margin-left:10px;" ><i data-feather="calendar" class="icon-16" ></i></span>
                 </div>
             </button>
         <?php
