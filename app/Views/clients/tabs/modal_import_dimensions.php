1282a7e1ba3d5aa781db83e913bed6ad4d9f555e
curest0x1021 <phillip.mr0x@gmail.com>
2024-04-12 21:37:44 +0200

    import modal for client dimensions

---
+++ app/Views/clients/tabs/modal_import_dimensions.php
@@ -0,0 +1,126 @@
+<div id="ajaxModalContent" >
+<style>
+    .dragover{
+        background-color:lightgray;
+    }
+</style>
+<div class="modal-body clearfix" id="panel-import-yard-xlsx">
+    <div class="container-fluid">
+        <?php echo form_open(get_uri("clients/import_dimensions"), array("id" => "task-form", "class" => "general-form", "role" => "form")); ?>
+        <?php echo form_close();?>
+        <div id="upload-area-cost-overview">
+            
+            <div class="d-flex justify-content-center align-items-center panel-drop-zone" style="height:20vh;border:1px dashed lightgray;" >
+                <div class="text-center" >    
+                    <p>Drag-and-drop documents here</p>
+                    <p>(or click to browse...)</p>
+                </div>
+            </div>
+        </div>
+        <input type="hidden" name="file_name" id="import_file_name" value="" />
+        <input type="file" hidden class="input-file-yard-items" />
+        <div id="preview-area"></div>
+    </div>
+</div>
+
+<div class="modal-footer">
+    <button type="button" class="btn btn-default cancel-upload" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
+</div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+       $(".panel-drop-zone").on("click",function(){
+        $(".input-file-yard-items").click();
+       });
+       $('#upload-area-task-cost-items').on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        })
+        .on('dragover dragenter', function() {
+            $(this).addClass('dragover');
+        })
+        .on('dragleave dragend drop', function() {
+            $(this).removeClass('dragover');
+        })
+        .on('drop', function(e) {
+            console.log(e.originalEvent.dataTransfer.files)
+            if(e.originalEvent.dataTransfer.files.length<1) return;
+            var myForm=new FormData();
+            myForm.append("client_id",<?php echo $client_id?>);
+            myForm.append("file",e.originalEvent.dataTransfer.files[0]);
+            var rise_csrf_token = $('[name="rise_csrf_token"]').val();
+            myForm.append("rise_csrf_token",rise_csrf_token);
+            $.ajax({
+                url:"<?php echo get_uri('clients/import_dimensions');?>",
+                method:"POST",
+                data:myForm,
+                contentType: false, // Set contentType to false, as FormData will automatically set the correct type
+                processData: false,
+                success:function(response){
+                    if(JSON.parse(response).success) {
+                        $maskTarget=$("#ajaxModalContent").find(".modal-body");
+                        var padding = $maskTarget.height() - 80;
+                        if (padding > 0) {
+                            padding = Math.floor(padding / 2);
+                        }
+                        $maskTarget.after("<div class='modal-mask'><div class='circle-loader'></div></div>");
+                        //check scrollbar
+                        var height = $maskTarget.outerHeight();
+                        $('.modal-mask').css({"width": $maskTarget.width() + 22 + "px", "height": height + "px", "padding-top": padding + "px"});
+                        $maskTarget.closest('.modal-dialog').find('[type="submit"]').attr('disabled', 'disabled');
+                        $maskTarget.addClass("hide");
+                        window.panel_import_yard_xlsx.closeModal()
+                        window.location.reload();
+                    }else{
+                        appAlert.error("That was an unvalid file.", {duration: 10000});
+                    }
+                
+                },
+                error:function(data){
+                    console.log(data)
+                    appAlert.error("That was an unvalid file.", {duration: 10000});
+                }
+            })
+        })
+       $(".input-file-yard-items").on('change',function(){
+        var myForm=new FormData();
+        myForm.append("client_id",<?php echo $client_id?>);
+        myForm.append("file",$(this)[0].files[0]);
+        var rise_csrf_token = $('[name="rise_csrf_token"]').val();
+        myForm.append("rise_csrf_token",rise_csrf_token);
+        $.ajax({
+            url:"<?php echo get_uri('clients/import_dimensions');?>",
+            method:"POST",
+            data:myForm,
+            contentType: false, // Set contentType to false, as FormData will automatically set the correct type
+            processData: false,
+            success:function(response){
+                if(JSON.parse(response).success) {
+                    $maskTarget=$("#ajaxModalContent").find(".modal-body");
+                    var padding = $maskTarget.height() - 80;
+                    if (padding > 0) {
+                        padding = Math.floor(padding / 2);
+                    }
+                    $maskTarget.after("<div class='modal-mask'><div class='circle-loader'></div></div>");
+                    //check scrollbar
+                    var height = $maskTarget.outerHeight();
+                    $('.modal-mask').css({"width": $maskTarget.width() + 22 + "px", "height": height + "px", "padding-top": padding + "px"});
+                    $maskTarget.closest('.modal-dialog').find('[type="submit"]').attr('disabled', 'disabled');
+                    $maskTarget.addClass("hide");
+                    window.panel_import_yard_xlsx.closeModal()
+                    window.location.reload();
+                }else{
+                    appAlert.error("That was an unvalid file.", {duration: 10000});
+                }
+            
+            },
+            error:function(data){
+                appAlert.error("That was an unvalid file.", {duration: 10000});
+            }
+        })
+       })
+       window.panel_import_yard_xlsx=$("#panel-import-yard-xlsx").appForm({
+        closeModalOnSuccess: false,
+       })
+    });
+</script>    
--- app/Views/clients/tabs/propulsion.php
+++ app/Views/clients/tabs/propulsion.php
@@ -1,149 +1,164 @@
 <div class="card" >
     <div class="card-body" >
-        <a class="btn btn-default" target="_blank" href="<?php echo get_uri("clients/export_propulsion/".$client_info->id);?>" ><i data-feather="download" class="icon-16"  ></i>Export Propulsion Info</a>
+    <div class="d-flex justify-content-end" >
+    <?php echo modal_anchor(get_uri("clients/modal_import_propulsion/".$client_info->id),'<button class="btn btn-default" target="_blank" ><i data-feather="upload" class="icon-16"  ></i>Import</button>',array());?>
+        <a class="btn btn-default" style="margin-left:1vw;" href="<?php echo get_uri("clients/export_propulsion/".$client_info->id);?>" target="_blank" ><i data-feather="download" class="icon-16"  ></i>Export</a>
+        </div>
         <div class="row" >
             <div class="col-md-4" >
-                <h3>Main Engine</h3>
-                <h4>
+                <h4>Main Engine</h4>
+                <div style="margin-left:2vw;" >
+                <p>
                     Maker : <?php echo $client_info->main_engine_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Model : <?php echo $client_info->main_engine_model;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Continuous output : <?php echo $client_info->main_engine_continuous_output;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Bore : <?php echo $client_info->main_engine_bore;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Stroke : <?php echo $client_info->main_engine_stroke;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Serial number : <?php echo $client_info->main_engine_serial_number;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Quantity : <?php echo $client_info->main_engine_quantity;?>
-                </h4>
+                </p>
+                </div>
             </div>
             <div class="col-md-2" ></div>
             <div class="col-md-4" >
-                <h3>Auxiliary Engine</h3>
-                <h4>
+                <h4>Auxiliary Engine</h4>
+                <div style="margin-left:2vw;" >
+                <p>
                     Maker : <?php echo $client_info->auxiliary_engine_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Model : <?php echo $client_info->auxiliary_engine_model;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Output : <?php echo $client_info->auxiliary_engine_output;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Serial number : <?php echo $client_info->auxiliary_engine_serial_number;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Quantity : <?php echo $client_info->auxiliary_engine_quantity;?>
-                </h4>
+                </p>
+                </div>
             </div>
         </div>
         <div class="row" >
             <div class="col-md-4" >
-                <h3>Emergency Generator</h3>
-                <h4>
+                <h4>Emergency Generator</h4>
+                <div style="margin-left:2vw;" >
+                <p>
                     Maker : <?php echo $client_info->emergency_generator_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Model : <?php echo $client_info->emergency_generator_model;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Output : <?php echo $client_info->emergency_generator_output;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Serial number : <?php echo $client_info->emergency_generator_serial_number;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Quantity : <?php echo $client_info->emergency_generator_quantity;?>
-                </h4>
+                </p>
+                </div>
             </div>
             <div class="col-md-2" ></div>
             <div class="col-md-4" >
-                <h3>Shaft Generator</h3>
-                <h4>
+                <h4>Shaft Generator</h4>
+                <div style="margin-left:2vw;" >
+                <p>
                     Maker : <?php echo $client_info->shaft_generator_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Model : <?php echo $client_info->shaft_generator_model;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Output : <?php echo $client_info->shaft_generator_output;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Serial number : <?php echo $client_info->shaft_generator_serial_number;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Quantity : <?php echo $client_info->shaft_generator_quantity;?>
-                </h4>
+                </p>
+                </div>
             </div>
         </div>
         <div class="row" >
             <div class="col-md-4" >
-                <h3>Propeller </h3>
-                <h4>
+                <h4>Propeller </h4>
+                <div style="margin-left:2vw;" >
+                <p>
                     Maker : <?php echo $client_info->propeller_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Type : <?php echo $client_info->propeller_type;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Number of blades : <?php echo $client_info->propeller_number_of_blades;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Diameter : <?php echo $client_info->propeller_diameter;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Pitch : <?php echo $client_info->propeller_pitch;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Material : <?php echo $client_info->propeller_material;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Weight : <?php echo $client_info->propeller_weight;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Output : <?php echo $client_info->propeller_output;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Quantity : <?php echo $client_info->propeller_quantity;?>
-                </h4>
+                </p>
+                </div>
             </div>
             <div class="col-md-2" ></div>
             <div class="col-md-4" >
-                <h3>Side Thruster</h3>
-                <h4>
+                <h4>Side Thruster</h4>
+                <div style="margin-left:2vw;" >
+                <p>
                     Number of bow thrusters : <?php echo $client_info->bow_thruster_number;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Maker of bow thrusters : <?php echo $client_info->bow_thruster_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Type of bow thrusters : <?php echo $client_info->bow_thruster_type;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Power of bow thrusters : <?php echo $client_info->bow_thruster_power;?>
-                </h4>
+                </p>
 
-                <h4>
+                <p>
                     Number of stern thrusters : <?php echo $client_info->stern_thruster_number;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Maker of stern thrusters : <?php echo $client_info->stern_thruster_maker;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Type of stern thrusters : <?php echo $client_info->stern_thruster_type;?>
-                </h4>
-                <h4>
+                </p>
+                <p>
                     Power of stern thrusters : <?php echo $client_info->stern_thruster_power;?>
-                </h4>
+                </p>
+                </div>
             </div>
         </div>
     </div>
