e286b32138d8fffa7944e03e7224e4129be9277e
curest0x1021 <phillip.mr0x@gmail.com>
2024-04-19 20:34:21 +0200

    widget troubleshoot

--- app/Views/projects/export_pdf/layouts/header.php
+++ app/Views/projects/export_pdf/layouts/header.php
@@ -1,10 +1,16 @@
 <table style="width:100%" >
     <tbody>
         <tr>
-            <td style="width:90%" >
-            <img src="<?php echo encode_img_base64(get_logo_url());?>" alt="logo"/>
+            <td style="width:40%" >
+                <img src="<?php echo encode_img_base64(get_logo_url());?>" alt="logo"/>
             </td>
-            <td >
+            <td style="width:20%">
+                <p><?php echo $project_info->title?></p>
+            </td>
+            <td style="width:30%">
+                <p><?php echo date("d.m.Y",strtotime($project_info->start_date))." ~ ".date("d.m.Y",strtotime($project_info->deadline));?></p>
+            </td>
+            <td style="width:10%" >
             <?php echo date('d.m.Y');?>
             </td>
         </tr>
