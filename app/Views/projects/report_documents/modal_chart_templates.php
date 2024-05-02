c902ccc9f7dd95ff25c834c17990af470a9f9cef
curest0x1021 <phillip.mr0x@gmail.com>
2024-05-02 04:46:08 +0200

    added chart

--- app/Views/projects/report_documents/modal_chart_templates.php
+++ app/Views/projects/report_documents/modal_chart_templates.php
@@ -4,9 +4,11 @@
     <button class="btn btn-default insert-chart-task-status" >Task status</button>
     <button class="btn btn-default insert-chart-project-progress" >Project progress</button>
     <button class="btn btn-default insert-chart-all-tasks-overview" >All tasks overview</button>
-    <!-- <div style="width:0px;height:0px;overflow:hidden;" > -->
+    <button class="btn btn-default insert-chart-my-tasks-overview" >All tasks overview</button>
+    <div style="overflow:hidden;" >
         <div class="preview" ></div>
-    <!-- </div> -->
+        <iframe id="iframe_box" ></iframe>
+    </div>
 </div>
 </div>
 <script src="<?php echo base_url("assets/js/html2canvas.min.js");?>" ></script>
@@ -49,23 +51,17 @@
                     preview_box.style.height="200vh"
                     preview_box.style.overflowX="hidden"
                     preview_box.style.overflowY="hidden"
-                    // virtual_box.hidden=true;
-                    // virtual_box.style.display="none";
                     
                     virtual_box.innerHTML=response;
                     var code=virtual_box.querySelector("script").innerHTML
                     preview_box.appendChild(virtual_box);
                     eval(code)
                     setTimeout(() => {
-                        //var image_dataURL=window.chart_project_progress.toBase64Image();
 
 
                         var element = document.getElementById('card-project-progress-chart');
                         element.style.height="40vh;"
                         html2canvas(element).then(canvas=>{
-                            // console.log(canvas)
-                            // canvas.width = element.width;
-                            // canvas.height = element.height;
                             const dataURL=canvas.toDataURL();
                             virtual_box.hidden=true;
                             const viewFragment = window.watchdog.editor.data.processor.toView( `<img src="${dataURL}" />` );
@@ -73,18 +69,6 @@
                             window.watchdog.editor.model.insertContent( modelFragment,window.watchdog.editor.model.document.selection.getFirstPosition());
                             window.modal_insert_chart.closeModal();
                         })
-                        // var canvas = document.createElement('canvas');
-                        // canvas.width = element.offsetWidth;
-                        // canvas.height = element.offsetHeight;
-
-                        // // Get the canvas context
-                        // var context = canvas.getContext('2d');
-
-                        // // Draw the div content onto the canvas
-                        // context.drawWindow(window, element.offsetLeft, element.offsetTop, element.offsetWidth, element.offsetHeight, 'rgb(255,255,255)');
-
-                        // // Convert the canvas to a data URL representing a PNG image
-                        // var dataURL = canvas.toDataURL('image/png');
 
                         
                     }, 800);
@@ -95,7 +79,7 @@
         $(".insert-chart-all-tasks-overview").on("click",function(){
             const virtual_box=document.createElement("div");
             const preview_box=document.body.querySelector(".preview");
-            var iframe_box=document.createElement("iframe");
+            var iframe_box=document.getElementById("iframe_box");
             preview_box.appendChild(iframe_box);
             iframe_box.style.height="50vh";
             iframe_box.style.width="50vw";
@@ -118,15 +102,34 @@
                 })
             }, 3000);
         })
+        $(".insert-chart-my-tasks-overview").on("click",function(){
+            const virtual_box=document.createElement("div");
+            const preview_box=document.body.querySelector(".preview");
+            var iframe_box=document.getElementById("iframe_box");
+            preview_box.appendChild(iframe_box);
+            iframe_box.style.height="50vh";
+            iframe_box.style.width="50vw";
+            iframe_box.style.overflow="hidden";
+            iframe_box.src="<?php echo get_uri("projects/tasks_overview_chart/my_tasks_overview");?>";
+            console.log(iframe_box)
+            
+            setTimeout(() => {
+                var iframe_document=iframe_box.contentDocument || iframe_box.contentWindow.document;
+                var element = iframe_document.getElementById('card-tasks-overview-chart');
+                console.log(element)
+                html2canvas(element).then(canvas=>{
+                    console.log(canvas)
+                    const dataURL=canvas.toDataURL();
+                    // virtual_box.hidden=true;
+                    const viewFragment = window.watchdog.editor.data.processor.toView( `<img src="${dataURL}" />` );
+                    const modelFragment = window.watchdog.editor.data.toModel( viewFragment );
+                    window.watchdog.editor.model.insertContent( modelFragment,window.watchdog.editor.model.document.selection.getFirstPosition());
+                    window.modal_insert_chart.closeModal();
+                })
+            }, 3000);
+        })
         window.modal_insert_chart=$("#modal-insert-chart").appForm({
             closeModalOnSuccess: false,
-            // onSuccess: function (result) {
-            //     if (window.continueShow) {
-            //         console.log(window.responseData)
-            //     }else{
-            //         window.edit_task_panel.closeModal()
-            //     }
-            // }
         });
     })
 </script>
