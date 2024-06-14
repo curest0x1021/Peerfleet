5f8d35a36352917e9dba421c1954fbb7f8d226d4
Curest0x1021 <phillip.mr0x@gmail.com>
2024-06-13 22:43:56 -0500

    Worked on Rework Ticket Module

--- app/Views/tickets/modal_form_new.php
+++ app/Views/tickets/modal_form_new.php
@@ -9,7 +9,18 @@
                     <label for="client_id" class=" col-md-3"><?php echo "Manufacturer"; ?></label>
                     <div class="col-md-9">
                         <?php
-                        echo form_dropdown("client_id", $clients_dropdown, array($model_info->client_id), "class='select2 validate-hidden' id='client_id' data-rule-required='true', data-msg-required='" . app_lang('field_required') . "'");
+                        echo form_input(
+                            array(
+                                "id" => "manufacturer_id",
+                                "name" => "manufacturer_id",
+                                "value" => $model_info->manufacturer ? $model_info->manufacturer : "",
+                                "class" => "form-control",
+                                "placeholder" => 'Manufacturer',
+                                "autofocus" => true,
+                                "data-rule-required" => true,
+                                "data-msg-required" => app_lang("field_required"),
+                            )
+                        );
                         ?>
                     </div>
                 </div>
@@ -19,16 +30,18 @@
                     <label for="equipment" class=" col-md-3"><?php echo "Equipment"; ?></label>
                     <div class=" col-md-9">
                         <?php
-                        echo form_input(array(
-                            "id" => "equipment",
-                            "name" => "equipment",
-                            "value" => $model_info->equipment?$model_info->equipment:"",
-                            "class" => "form-control",
-                            "placeholder" => 'Equipment',
-                            "autofocus" => true,
-                            "data-rule-required" => true,
-                            "data-msg-required" => app_lang("field_required"),
-                        ));
+                        echo form_input(
+                            array(
+                                "id" => "equipment",
+                                "name" => "equipment",
+                                "value" => $model_info->equipment ? $model_info->equipment : "",
+                                "class" => "form-control",
+                                "placeholder" => 'Equipment',
+                                "autofocus" => true,
+                                "data-rule-required" => true,
+                                "data-msg-required" => app_lang("field_required"),
+                            )
+                        );
                         ?>
                     </div>
                 </div>
@@ -38,16 +51,18 @@
                     <label for="serial_number" class=" col-md-3"><?php echo "Serial Number"; ?></label>
                     <div class=" col-md-9">
                         <?php
-                        echo form_input(array(
-                            "id" => "serial_number",
-                            "name" => "serial_number",
-                            "value" => $model_info->serial_number?$model_info->serial_number:"",
-                            "class" => "form-control",
-                            "placeholder" => 'Serial Number',
-                            "autofocus" => true,
-                            "data-rule-required" => true,
-                            "data-msg-required" => app_lang("field_required"),
-                        ));
+                        echo form_input(
+                            array(
+                                "id" => "serial_number",
+                                "name" => "serial_number",
+                                "value" => $model_info->serial_number ? $model_info->serial_number : "",
+                                "class" => "form-control",
+                                "placeholder" => 'Serial Number',
+                                "autofocus" => true,
+                                "data-rule-required" => true,
+                                "data-msg-required" => app_lang("field_required"),
+                            )
+                        );
                         ?>
                     </div>
                 </div>
@@ -57,26 +72,28 @@
                     <label for="critical_disturbance" class=" col-md-3"><?php echo "Critical Disturbance"; ?></label>
                     <div class=" col-md-9">
                         <?php
-                        $critical_disturbances=[
+                        $critical_disturbances = [
                             [
-                                "id"=>0,
-                                "text"=>"No"
+                                "id" => 0,
+                                "text" => "No"
                             ],
                             [
-                                "id"=>1,
-                                "text"=>"Yes"
+                                "id" => 1,
+                                "text" => "Yes"
                             ],
                         ];
-                        echo form_input(array(
-                            "id" => "critical_disturbance",
-                            "name" => "critical_disturbance",
-                            "value" => $model_info->critical_disturbance?$model_info->critical_disturbance:"",
-                            "class" => "form-control",
-                            "placeholder" => 'Critical Disturbance',
-                            "autofocus" => true,
-                            "data-rule-required" => true,
-                            "data-msg-required" => app_lang("field_required"),
-                        ));
+                        echo form_input(
+                            array(
+                                "id" => "critical_disturbance",
+                                "name" => "critical_disturbance",
+                                "value" => $model_info->critical_disturbance ? $model_info->critical_disturbance : "",
+                                "class" => "form-control",
+                                "placeholder" => 'Critical Disturbance',
+                                "autofocus" => true,
+                                "data-rule-required" => true,
+                                "data-msg-required" => app_lang("field_required"),
+                            )
+                        );
                         ?>
                     </div>
                 </div>
@@ -86,37 +103,39 @@
                     <label for="critical_equipment" class=" col-md-3"><?php echo "Critical Equipment"; ?></label>
                     <div class=" col-md-9">
                         <?php
-                        $critical_equipments=[
+                        $critical_equipments = [
                             [
-                                "id"=>0,
-                                "text"=>"No"
+                                "id" => 0,
+                                "text" => "No"
                             ],
                             [
-                                "id"=>1,
-                                "text"=>"Yes"
+                                "id" => 1,
+                                "text" => "Yes"
                             ],
                         ];
-                        echo form_input(array(
-                            "id" => "critical_equipment",
-                            "name" => "critical_equipment",
-                            "value" => $model_info->critical_equipment?$model_info->critical_equipment:"",
-                            "class" => "form-control",
-                            "placeholder" => 'Critical Equipment',
-                            "autofocus" => true,
-                            "data-rule-required" => true,
-                            "data-msg-required" => app_lang("field_required"),
-                        ));
+                        echo form_input(
+                            array(
+                                "id" => "critical_equipment",
+                                "name" => "critical_equipment",
+                                "value" => $model_info->critical_equipment ? $model_info->critical_equipment : "",
+                                "class" => "form-control",
+                                "placeholder" => 'Critical Equipment',
+                                "autofocus" => true,
+                                "data-rule-required" => true,
+                                "data-msg-required" => app_lang("field_required"),
+                            )
+                        );
                         ?>
                     </div>
                 </div>
             </div>
             <!------------------------------------------>
             <!-- client can't be changed during editing -->
-            
 
-            
 
-          
+
+
+
 
             <?php echo view("custom_fields/form/prepare_context_fields", array("custom_fields" => $custom_fields, "label_column" => "col-md-3", "field_column" => " col-md-9")); ?>
 
@@ -128,13 +147,16 @@
 
         <!-- file can't be uploaded during editing -->
         <button class="btn btn-default upload-file-button float-start me-auto btn-sm round  <?php
-                                                                                            if ($model_info->id) {
-                                                                                                echo "hide";
-                                                                                            }
-                                                                                            ?>" type="button" style="color:#7988a2"><i data-feather='camera' class='icon-16'></i> <?php echo app_lang("upload_file"); ?></button>
+        if ($model_info->id) {
+            echo "hide";
+        }
+        ?>" type="button" style="color:#7988a2"><i data-feather='camera' class='icon-16'></i>
+            <?php echo app_lang("upload_file"); ?></button>
 
-        <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x" class="icon-16"></span> <?php echo app_lang('close'); ?></button>
-        <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('save'); ?></button>
+        <button type="button" class="btn btn-default" data-bs-dismiss="modal"><span data-feather="x"
+                class="icon-16"></span> <?php echo app_lang('close'); ?></button>
+        <button type="submit" class="btn btn-primary"><span data-feather="check-circle" class="icon-16"></span>
+            <?php echo app_lang('save'); ?></button>
     </div>
 </div>
 <?php echo form_close(); ?>
@@ -142,7 +164,7 @@
 
 
 <script type="text/javascript">
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('#description').summernote();
         var uploadUrl = "<?php echo get_uri("tickets/upload_file"); ?>";
         var validationUrl = "<?php echo get_uri("tickets/validate_ticket_file"); ?>";
@@ -151,7 +173,7 @@
         var dropzone = attachDropzoneWithForm("#new-ticket-dropzone", uploadUrl, validationUrl);
 
         $("#ticket-form").appForm({
-            onSuccess: function(result) {
+            onSuccess: function (result) {
                 if (editMode) {
 
                     appAlert.success(result.message, {
@@ -176,90 +198,9 @@
 
             }
         });
-        setTimeout(function() {
+        setTimeout(function () {
             $("#title").focus();
         }, 200);
         $("#ticket-form .select2").select2();
-
-        $("#ticket_labels").select2({
-            multiple: true,
-            data: <?php echo json_encode($label_suggestions); ?>
-        });
-
-        <?php if ($show_project_reference == "1") { ?>
-            //load all projects of selected client
-            $("#client_id").select2().on("change", function() {
-                var client_id = $(this).val();
-                if ($(this).val()) {
-                    $('#project_id').select2("destroy");
-                    $("#project_id").hide();
-                    appLoader.show({
-                        container: "#porject-dropdown-section",
-                        zIndex: 1
-                    });
-                    $.ajax({
-                        url: "<?php echo get_uri("tickets/get_project_suggestion") ?>" + "/" + client_id,
-                        dataType: "json",
-                        success: function(result) {
-                            $("#project_id").show().val("");
-                            $('#project_id').select2({
-                                data: result
-                            });
-                            appLoader.hide();
-                        }
-                    });
-                }
-            });
-
-            $('#project_id').select2({
-                data: <?php echo json_encode($projects_suggestion); ?>
-            });
-
-        <?php } ?>
-
-        //load all client contacts of selected client
-        $("#client_id").select2().on("change", function() {
-            var client_id = $(this).val();
-            if ($(this).val()) {
-                $('#requested_by_id').select2("destroy");
-                $("#requested_by_id").hide();
-                appLoader.show({
-                    container: "#requested-by-dropdown-section",
-                    zIndex: 1
-                });
-                $.ajax({
-                    url: "<?php echo get_uri("tickets/get_client_contact_suggestion") ?>" + "/" + client_id,
-                    dataType: "json",
-                    success: function(result) {
-                        $("#requested_by_id").show().val("");
-                        $('#requested_by_id').select2({
-                            data: result
-                        });
-                        appLoader.hide();
-                    }
-                });
-            }
-        });
-
-        $('#requested_by_id').select2({
-            data: <?php echo json_encode($requested_by_dropdown); ?>
-        });
-
-        $("#critical_disturbance").select2({
-            data: <?php 
-                echo (json_encode($critical_disturbances)); 
-                ?>
-        });
-
-        $("#critical_equipment").select2({
-            data: <?php 
-                echo (json_encode($critical_equipments)); 
-                ?>
-        });
-
-        if ("<?php echo $project_id; ?>") {
-            $("#client_id").select2("readonly", true);
-        }
-
     });
 </script>
