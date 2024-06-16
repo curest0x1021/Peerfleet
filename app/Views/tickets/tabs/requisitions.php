d1c9620768f386677ac1d351c2c7dcec4ee00ee3
Curest0x1021 <phillip.mr0x@gmail.com>
2024-06-16 12:03:24 -0500

    Added check condition if requisition_number is null or not

--- app/Views/tickets/tabs/requisitions.php
+++ app/Views/tickets/tabs/requisitions.php
@@ -4,24 +4,26 @@
         <div class="box-title"><span>Requisitions</span></div>
         <?php foreach ($allActions as $key => $action) {
         ?>
-            <button class='btn btn-default btn-lg mb-1 w-100' data-act="ajax-modal" data-title="Edit Corrective Action" data-action-url="<?php echo get_uri("tickets/modal_corrective_action/".$action->id);?>"  >
-                <div class="text-start" >
-                    <!-- <h5>&nbsp;Action : <?php echo $action->task_title;?></h5> -->
-                    <h5><?php echo $action->corrective_action;?></h5>
-                </div>
-                <div class="row text-start" >
-                    <div class="col-md-4" >
-                        <p>
-                            Title : <?php if(isset($action)) echo $action->requisition_title;?>
-                        </p>
+            <?php if(isset($action)&&$action->requisition_number){ ?>
+                <button class='btn btn-default btn-lg mb-1 w-100' data-act="ajax-modal" data-title="Edit Corrective Action" data-action-url="<?php echo get_uri("tickets/modal_corrective_action/".$action->id);?>"  >
+                    <div class="text-start" >
+                        <!-- <h5>&nbsp;Action : <?php echo $action->task_title;?></h5> -->
+                        <h5><?php echo $action->corrective_action;?></h5>
                     </div>
-                    <div class="col-md-4" >
-                        <p>
-                            Number : <?php if(isset($action)) echo $action->requisition_number;?>
-                        </p>
+                    <div class="row text-start" >
+                        <div class="col-md-4" >
+                            <p>
+                                Title : <?php if(isset($action)) echo $action->requisition_title;?>
+                            </p>
+                        </div>
+                        <div class="col-md-4" >
+                            <p>
+                                Number : <?php if(isset($action)) echo $action->requisition_number;?>
+                            </p>
+                        </div>
                     </div>
-                </div>
-            </button>
+                </button>
+            <?php }?>
         <?php
         } ?>
     </div>
